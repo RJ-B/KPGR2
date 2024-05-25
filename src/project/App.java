0e9aa12b380478fc086fbe4414310882ea02ce98
RJ-B <rosta1013@gmail.com>
2024-05-25 12:03:04 +0200

    Delete src directory

--- src/project/App.java
+++
@@ -1,7 +0,0 @@
-package project;
-
-public class App {
-    public static void main(String[] args) {
-        new global.LwjglWindow(new Renderer(), false);
-    }
-}
--- src/project/utils/types/Disposable.java
+++
@@ -1,5 +0,0 @@
-package project.utils.types;
-
-public interface Disposable {
-    void dispose();
-}
